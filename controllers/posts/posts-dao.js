673666e238b9269084d71c71c93ae484a68d6b5f
alxqiu <alxqiu@gmail.com>
2023-04-14 18:56:45 -0400

    updating schemas for posts and users, made artist and listener schema into one schema

--- controllers/posts/posts-dao.js
+++ controllers/posts/posts-dao.js
@@ -1,7 +1,7 @@
 import postsModel from './posts-model.js';
 export const findPosts = () => postsModel.find();
 export const findPost = (pid) => postsModel.find({_id: pid});
-export const findFollowingPosts = (uids) => postsModel.find({userId: {$in: uids}});
+export const findFollowingPosts = (uids) => postsModel.find({artistId: {$in: uids}});
 export const createPost = (post) => postsModel.create(post);
 export const deletePost = (pid) => postsModel.deleteOne({_id: pid});
 export const updatePost = (pid, post) => postsModel.updateOne({_id: pid}, {$set: post});
