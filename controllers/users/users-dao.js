26832da826ef2142f93c43486095b163712a19c3
alxqiu <alxqiu@gmail.com>
2023-04-22 19:35:36 -0400

    added findUserByUsername

--- controllers/users/users-dao.js
+++ controllers/users/users-dao.js
@@ -1,6 +1,9 @@
 import usersModel from './users-model.js';
 export const findUserByCredentials = (username, password) =>
     usersModel.findOne({ username, password });
+
+export const findUserByUsername = (username) =>
+    usersModel.findOne({username});
 export const findUser = (uid) => usersModel.findById(uid);
 
 export const findUsers = () => usersModel.find();
